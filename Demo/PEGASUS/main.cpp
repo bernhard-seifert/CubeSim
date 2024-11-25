9106b83334f27236b0c48e7f20786d23b7423fff
Bernhard Seifert <bernhard.seifert@siliconsystems.at>
2024-11-25 21:20:03 +0100

    removed configuration x64 fixed spacecraft orbit initialization added more comments

--- Demo/PEGASUS/main.cpp
+++ Demo/PEGASUS/main.cpp
@@ -32,8 +32,9 @@ void PegasusADCS::_init(void)
 
    // Compute Spacecraft Orbit: 400 km, 0.01 Eccentricity, 70° Inclination
    CubeSim::Orbit orbit(earth, (6370 + 400) * 1000, 0.01, 120 * CubeSim::Constant::PI / 180,
-      50 * CubeSim::Constant::PI / 180, 70 * CubeSim::Constant::PI / 180, 20.0 * CubeSim::Constant::PI / 180,
-      simulation()->time(), CubeSim::Orbit::REFERENCE_ECI);
+      50 * CubeSim::Constant::PI / 180, 70 * CubeSim::Constant::PI / 180, 20.0 * CubeSim::Constant::PI / 180, Time(),
+      CubeSim::Orbit::REFERENCE_ECI);
+   orbit.time(simulation()->time());
 
    // Initialize Spacecraft Position, Velocity, Orientation, angular Rate
    spacecraft.position(earth.position() + orbit.position());
@@ -44,10 +45,10 @@ void PegasusADCS::_init(void)
 // Main
 int main(void)
 {
-   // Pegasus ADCS System
+   // Set Up PEGASUS ADCS System
    PegasusADCS adcs;
 
-   // Insert six Visible Light Photo Diodes
+   // Set Up six Visible Light Photo Diodes and insert them into ADCS System
    TEMD6200FX01 photo_diode;
    photo_diode.position(-0.05, 0.00, 0.00);
    photo_diode.rotation(CubeSim::Vector3D::Y, -CubeSim::Constant::PI / 2.0);
@@ -68,23 +69,23 @@ int main(void)
    photo_diode.rotation(CubeSim::Rotation());
    adcs.insert("PhotoDiodeZP", photo_diode);
 
-   // Insert Magnetometer
+   // Set up Magnetometer and insert it into ADCS System
    HMC5983 magnetometer;
    adcs.insert("Magnetometer", magnetometer);
 
-   // Insert Gyroscope
+   // Set up Gyroscope and insert it into ADCS System
    L3G4200D gyroscope;
    adcs.insert("Gyroscope", gyroscope);
 
-   // Insert Accelerometer
+   // Set up Accelerometer and insert it into ADCS System
    ADXL330 accelerometer;
    adcs.insert("Accelerometer", accelerometer);
 
-   // Insert GPS
+   // Set up GPS and insert it into ADCS System
    OEM615 gps;
    adcs.insert("GPS", gps);
 
-   // Insert Sidepanel Magnetorquers
+   // Set Up Sidepanel Magnetorquers and insert them into ADCS System
    SidepanelTorquer sidepanel_torquer;
    sidepanel_torquer.position(-0.05, 0.00, 0.00);
    sidepanel_torquer.rotation(CubeSim::Vector3D::Y, -CubeSim::Constant::PI / 2.0);
@@ -99,33 +100,27 @@ int main(void)
    sidepanel_torquer.rotation(CubeSim::Vector3D::X, -CubeSim::Constant::PI / 2.0);
    adcs.insert("MagnetorquerYP", sidepanel_torquer);
 
-   // Insert Adapter Board Magnetorquer
+   // Set Up Adapter Board Magnetorquer and insert it into ADCS System
    AdapterTorquer adapter_torquer;
    adapter_torquer.position(0.00, 0.00, 0.10);
    adapter_torquer.rotation(CubeSim::Rotation());
    adcs.insert("MagnetorquerZ", adapter_torquer);
 
-   // Spacecraft Bus
+   // Set Up Spacecraft Bus
    CubeSim::Part::Box bus_part(0.1, 0.1, 0.2);
    bus_part.material(CubeSim::Material("", 1000.0));
    CubeSim::Assembly bus_assembly;
    bus_assembly.insert("Bus", bus_part);
    adcs.insert("Bus", bus_assembly);
 
-   // Monitor System
-   Monitor monitor;
-
-   // Interface System
-   ADCS::Interface interface_;
-
-   // Spacecraft
+   // Set Up Spacecraft
    CubeSim::Spacecraft spacecraft;
    spacecraft.position(1E9, 1E9, 1E9);
    spacecraft.insert("ADCS", adcs);
-   spacecraft.insert("Monitor", monitor);
-   spacecraft.insert("Interface", interface_);
+   spacecraft.insert("Monitor", Monitor());
+   spacecraft.insert("Interface", ADCS::Interface());
 
-   // Simulation
+   // Set Up Simulation
    CubeSim::Simulation simulation;
    simulation.insert("Spacecraft", spacecraft);
    CubeSim::CelestialBody& sun = simulation.insert("Sun", CubeSim::CelestialBody::Sun());
@@ -139,10 +134,10 @@ int main(void)
    simulation.insert("Light", CubeSim::Module::Light());
    simulation.insert("Albedo", CubeSim::Module::Albedo(earth));
 
-   // Set Simulation Time
-   simulation.time(Time(2017, 6, 23));
+   // Set Simulation Time to Launch Date
+   simulation.time(Time(2017, 6, 23, 0, 30));
 
-   // Run Simulation
+   // Run Simulation for 1 Year
    simulation.run(3600 * 24 * 365);
 
    // Return
